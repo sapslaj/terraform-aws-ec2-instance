756e98e8d7e76da33818c84a408e3fa1a60fc003
Justin Roberson <1909600+sapslaj@users.noreply.github.com>
2024-08-06 13:30:28 -0400

    don't install awscli in bootstrap-self.sh
    Because the awscli package doesn't really exist

--- ansible/bootstrap-self.sh
+++ ansible/bootstrap-self.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 if command -v apt-get &>/dev/null ; then
   apt-get update
-  apt-get install -y git awscli ansible
+  apt-get install -y git ansible
 else
   echo "Unsupported platform: $(lsb_release -a)"
   exit 1
